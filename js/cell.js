0585b408597373536d4728f7372b87e9a9c15c82
Olivia Liebman <olivialiebman@yahoo.com>
2017-01-08 15:41:36 +0200

    working version, needs click functionalities

---
+++ js/cell.js
@@ -0,0 +1,44 @@
+class Cell {
+    constructor(element, clickCb) {
+        this.element = element;
+        this.element.on('click', function(){
+            clickCb(this);
+        }.bind(this)); //'this' in this context: refers to the specific cell (with all its functions) that triggered the click event
+        this.setBackgroundColor('#79a9f7');
+    }
+    isClickable(toolId) {
+        if (this.imgClass === 'leaf' && toolId === 'axe') {
+            console.log('leaf');
+            return true;
+        }
+        if (this.imgClass === 'trunk' && toolId === 'axe') {
+            console.log(this.imgClass);
+            console.log('trunk');
+            return true;
+        }
+        if (this.imgClass === 'stone' && toolId === 'pickaxe') {
+            console.log('stone');
+            return true;
+        }
+        if (this.imgClass === 'dirt' && toolId === 'shovel') {
+            console.log('dirt');
+            return true;
+        }
+        if (this.imgClass === 'topDirt' && toolId === 'shovel') {
+            console.log('topDirt');
+            return true;
+        }
+        return false;
+    }
+    setClass(imgClass) {
+        this.imgClass = imgClass;
+        this.element.addClass(imgClass);
+    }
+    setBackgroundColor(backgroundColor) {
+        this.element.css('background-color', backgroundColor);
+    }
+    setBackgroundImg(backgroundImg) {
+        this.element.css('background', backgroundImg).css('background-size', 'cover');
+        return this;
+    }
+}
---
+++ js/minecraft.js
@@ -0,0 +1,83 @@
+
+'use strict';
+
+var GameManager = function() {
+    this.cells = this.initGame();
+};
+
+GameManager.prototype.initGame = function() {
+    var cells = this._initGrid();
+    this._setUpGame(cells);
+    return cells;
+};
+
+GameManager.prototype._initGrid = function() {
+    var cells = {};
+    for (var i = 0; i <20; i++){//i = y coord
+        $(".grid").append('<div class="row"></div>');
+
+        for (var j = 0; j<20; j++){
+            $(".row").eq(i).append('<div class="cell"></div>');
+            //make an instance of Cell called Cells[key] where key is the xy of the cell
+            cells[j+''+i] = new Cell($(".row:eq("+i+") .cell").eq(j).attr("id", j+"-"+i), this._clickedCell.bind(this));//j = x coord. Makes id: #j-i
+
+        }
+    }
+    return cells;
+};
+
+GameManager.prototype._clickedCell = function(cell) {
+    console.log(cell);
+
+};
+
+GameManager.prototype._setUpGame = function(cells) {
+    this._makeCloud(cells);
+    this._makeStone(cells);
+    this._makeLeaf(cells);
+    this._makeTrunk(cells);
+    this._makeDirt(cells);
+};
+GameManager.prototype._makeCloud = function(cells) {
+    var cloudArr = ['75', '46', '56', '66', '76', '86', '106', '116', '116', '37', '47', '57', '67', '77', '87', '97', '107', '117', '88', '98'];
+
+    for (let i = 0; i < cloudArr.length; i++) {
+        let index = cloudArr[i];
+        cells[index].setBackgroundColor('white');
+    }
+};
+GameManager.prototype._makeStone = function(cells) {
+    var stoneArr = ['1913', '1413', '1313'];
+    for (let i = 0; i < stoneArr.length; i++) {
+        let index = stoneArr[i];
+        cells[index].setBackgroundImg('url(./images/stone.jpg)').setClass('stone');
+    }
+};
+GameManager.prototype._makeTrunk = function(cells) {
+    var trunkArr = ['1613', '1612', '1611'];
+    for (let i = 0; i < trunkArr.length; i++) {
+        let index = trunkArr[i];
+        cells[index].setBackgroundImg('url(./images/tree.jpg)').setClass('trunk');
+    }
+};
+GameManager.prototype._makeLeaf = function(cells) {
+    var leafArr = ['313', '413', '513', '412', '1510', '1610', '1710', '159', '169', '179', '158', '168', '178'];
+    for (let i = 0; i < leafArr.length; i++) {
+        let index = leafArr[i];
+        cells[index].setBackgroundImg('url(./images/leaf.jpg)').setClass('leaf');
+    }
+};
+GameManager.prototype._makeDirt = function(cells) {
+
+    for (var m = 14; m < 20; m++) {
+        for (var n = 0; n < 20; n++) {
+            let index = '' + n + m + '';
+            if (m === 14)
+            cells[index].setBackgroundImg('url(./images/topDirt.png)').setClass('topDirt');
+            else
+                cells[index].setBackgroundImg('url(./images/dirt.jpg)').setClass('dirt');
+        }
+    }
+};
+
+var game = new GameManager();
